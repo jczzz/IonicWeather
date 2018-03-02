6fa4712f797048953e574f9eb67014432e13739a
jczzz <sharpshui@hotmail.com>
2018-03-02 01:42:55 -0800

    first commit

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,29 @@
+
+import { Injectable } from '@angular/core';
+import { Http,Headers, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+
+  apiKey = '99dfe35fcb7de1ee';
+  url;
+
+
+  //构造器，   实例中都含内部设定
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+      .map(res => res.json());
+  }
+
+}
